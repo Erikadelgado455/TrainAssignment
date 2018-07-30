8227cc8d48833a8ae659561ca5da2ecd53793f75
erikadelgado455 <erikadelgado455@yahoo.com>
2018-07-30 05:34:00 -0400

    i created my javascript file and i added my firebase config

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyDqrx8obfUvmQ9TLcd4Tn7C2CFlJX-rIR0",
+        authDomain: "train-scheduler-95439.firebaseapp.com",
+        databaseURL: "https://train-scheduler-95439.firebaseio.com",
+        projectId: "train-scheduler-95439",
+        storageBucket: "train-scheduler-95439.appspot.com",
+        messagingSenderId: "779973586607"
+
+    }
+})
